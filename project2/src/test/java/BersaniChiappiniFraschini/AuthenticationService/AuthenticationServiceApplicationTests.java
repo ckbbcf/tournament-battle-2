9ecc4fdef2e14f7823a57a3f73f48e166585e009
ckbbcf <code.kata.battle.git@github.com>
2024-01-29 23:12:49 +0100

    Added the code for the battle /src/test/java/BersaniChiappiniFraschini/AuthenticationService/AuthenticationServiceApplicationTests.java

---
+++ project2/src/test/java/BersaniChiappiniFraschini/AuthenticationService/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package BersaniChiappiniFraschini.AuthenticationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+}
